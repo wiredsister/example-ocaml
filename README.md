ba3edcd0df6e268344310d4e047deeda310ba105
Gina Maini <drawginadraw@gmail.com>
2016-02-08 14:05:27 -0800

    Example complete.

--- README.md
+++ README.md
@@ -1,8 +1,7 @@
-# sam
-Basic local server that gives you a JSON pretty-printed list of Processes with pids, usr, and commands in OCaml
-
+# Example OCaml Meetup
+Bread and butter OCaml program that makes an HTTP request and prints the response to the console.
 
 To run, do:
 `make`
 then 
-`./sam` and separately, do `curl localhost:5000` for JSON response
+`./example`
--- main.ml
+++ main.ml
@@ -1,21 +1,44 @@
 open Lwt.Infix
 open Cohttp_lwt_unix
 
-let request =
-  (* Client.get (Uri.of_string "http://api.coindesk.com/v1/bpi/currentprice/usd.json") *)
+(*** Here is an example of a bread and butter OCaml program
+     that makes an HTTP GET request (using Cohttp) and then
+     prints that to the screen.
+***)
+
+let request () =
+  (* Some bitcoin price information. *)
+  (* Client.get :
+     ?ctx:Cohttp_lwt_unix.Client.ctx ->
+     ?headers:Cohttp.Header.t ->
+     Uri.t ->
+     (Cohttp.Response.t * Cohttp_lwt_body.t) Lwt.t *)
   Client.get (Uri.of_string "http://www.bitstamp.net/api/ticker/")
-  >>= fun (resp, body)->
+
+  (*** Bind, or as some affectionally call shove: 'a Lwt.t -> (a' -> b' Lwt.t) -> 'b Lwt.t
+     What we're doing here is shoving the thing inside the monad on the left of the >>=
+     into the right side of the >>= then wrapping that result in a monad.
+     Another way to think about this might be:
+     "I don't know when the left side will happen, but when it does, do this with it."
+  ***)
+  >>= fun (resp, body) ->
   let code = resp |> Response.status |> Cohttp.Code.code_of_status in
   Printf.printf "Response code: %d\n" code;
   Printf.printf "Headers: %s\n" (resp |> Response.headers |> Cohttp.Header.to_string);
   body |> Cohttp_lwt_body.to_string >|= fun body ->
   Printf.printf "Body of length: %d\n" (String.length body);
-  Yojson.Basic.Util.member "time" body  |> Yojson.to_string
-
-
-(* let time = *)
-(*   ODate.Unix.From.seconds  *)
+  body
 
 let () =
-  let bitcoin_price = Lwt_main.run request in
+  (*** Lwt is a cooperative threading library in OCaml.
+       Cooperative threads (A.K.A. non-premtive threads)
+       typically yield control to another thread before they finish.
+       People use Lwt for I/O, events, and ease of use.
+       See also, async. ***)
+
+  (* Lwt_main.run : 'a Lwt.t -> 'a *)
+  (* run t calls the Lwt scheduler repeatedly until t terminates,
+     then returns the value returned by the thread.
+     If t fails with an exception the exception is raised. *)
+  let bitcoin_price = Lwt_main.run (request ()) in
   print_endline ("Received response\n" ^ bitcoin_price)
